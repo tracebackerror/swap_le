7a90763c8abc27d213872136dfa8e97f2a2cf5e0
tracebackerror <shivam9172@gmail.com>
2018-01-31 02:03:59 +0530

    Deployment Settings

--- swaple/settings_bu.py
+++ swaple/settings_bu.py
@@ -92,8 +92,10 @@ WSGI_APPLICATION = 'swaple.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600,
-                                      default='sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
