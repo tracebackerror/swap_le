36db85a7faf8c8d949b06c2e1c682b1a32cdf2be
tracebackerror <shivam9172@gmail.com>
2018-03-07 03:26:00 +0530

    Updated Home Urls

--- swaple/urls.py
+++ swaple/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^institutions/', include('institutions.urls', namespace='institutions', app_name='institutions')),
     url(r'^staff/', include('staff.urls', namespace='staff', app_name='staff')),
     url(r'^student/', include('students.urls', namespace='student', app_name='student')),
+    url(r'^$', include('home.urls', namespace='home', app_name='home'))
     
 ]
