9db7cc94957414a50f9bd2fb2dd927ba7631127f
V!NOD <V!NOD@DESKTOP-8UQ2J9K>
2018-05-16 17:33:57 +0530

    Issue #57 : Implemented contact us form submission on Home Page.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 from django.views.generic import TemplateView
+from .models import ContactUs
+from django.contrib import messages
 
 class HomePageView(TemplateView):
     template_name = "home/index.html"
+    
+def contactus(request):
+    if request.method=="GET":
+        iname=request.GET['name']
+        iemail=request.GET['email']
+        isubject=request.GET['subject']
+        imessage=request.GET['message']
+        ContactUs.objects.create(name=iname,email=iemail,subject=isubject,message=imessage)
+        messages.add_message(request, messages.SUCCESS, 'Feedback Sent Successfully')
+    return HttpResponseRedirect('/')
