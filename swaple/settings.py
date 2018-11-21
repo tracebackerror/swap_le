42fbdb93d96939be4adfb00705a97af71616c685
tracebackerror <shivam9172@gmail.com>
2018-11-21 14:21:55 +0530

    ss

--- swaple/settings.py
+++ swaple/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'guardian',
     'django_tables2',
     'django_filters',
--- swaple/urls.py
+++ swaple/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from home.views import contactus
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -12,5 +13,5 @@ urlpatterns = [
     url(r'^$', include('home.urls', namespace='home', app_name='home')),
     url(r'^contactus/',contactus,name='contactus'),
     url(r'^google0f6eb0891016c158\.html$', TemplateView.as_view(template_name='google0f6eb0891016c158.html', content_type='text/plain')), 
-    
+   
 ]
