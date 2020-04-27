c16dc4fa50854bfcf24aee8f7aad9ec166273cf1
Shivam Chaurasia <shivam9172@gmail.com>
2020-04-27 13:26:15 +0530

    fix trial 2: conditional logi

--- assesments/views.py
+++ assesments/views.py
@@ -418,7 +418,8 @@ class ManageStudentAssesmentView(SingleTableView, ListView):
         
         #self.queryset = Assesment.objects.filter(subscriber_users = student_obj, privilege='public').filter(Q(result__assesment_submitted=False) |  Q(result__isnull=True))
         all_user_linked_assesment = Assesment.objects.filter(subscriber_users = student_obj, privilege='public')
-        all_user_linked_assesment_filter_exam_date = all_user_linked_assesment.filter(exam_start_date_time__lte= timezone.localtime(timezone.now()), expired_on__gte= timezone.localtime(timezone.now()))
+        #all_user_linked_assesment_filter_exam_date = all_user_linked_assesment.filter(exam_start_date_time__lte= timezone.localtime(timezone.now()), expired_on__gte= timezone.localtime(timezone.now()))
+        all_user_linked_assesment_filter_exam_date = all_user_linked_assesment.filter(exam_start_date_time__gte= timezone.localtime(timezone.now()), expired_on__lte= timezone.localtime(timezone.now()))
 
         if all_user_linked_assesment_filter_exam_date.exists():
             all_user_linked_result = Result.objects.filter(registered_user=student_obj).filter(assesment_submitted=True)
