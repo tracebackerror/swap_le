343eb8956ce5f7321d037ff5dfd2fe5ee9f73bfd
Shivam Chaurasia <shivam9172@gmail.com>
2020-05-24 08:48:49 +0530

    ads removal

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
    
     url(r'^google0f6eb0891016c158\.html$', GoogleIndexViewVerification.as_view(), name="googleindexview"),
     
-    url(r'^sw.js', PropellerAds.as_view(), name="propellerads"),
-     url(r'^', HomePageView.as_view(), name="homeview"),
+    #url(r'^sw.js', PropellerAds.as_view(), name="propellerads"),
+    url(r'^', HomePageView.as_view(), name="homeview"),
     ]
